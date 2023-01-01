2117cf826abd2fd8262f8c235378b8185179c6e9
xanderkiev2022 <113243861+xanderkiev2022@users.noreply.github.com>
2023-01-01 21:08:08 +0000

    Deploying to gh-pages from @ xanderkiev2022/CocktailsTeamProject@49120766f5d9119fe62f0b0b724b31da3588552f 🚀

--- index.d9c85ff9.js
+++
@@ -1,2 +0,0 @@
-const t="https://www.thecocktaildb.com/api/json/v1/";(async function(o){const c=`${t}/1/search.php?s=${o}`;try{const t=await fetch(c);return await t.json()}catch{console.log(error.message)}})("margarita").then((t=>console.log(t)));
-//# sourceMappingURL=index.d9c85ff9.js.map
