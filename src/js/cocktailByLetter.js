0f8834822795679324f78fcf751c99893532cac7
Yuriy Minashkin <y.minashkin@gmail.com>
2023-01-03 12:41:28 +0200

    Update cocktailByLetter.js

--- src/js/cocktailByLetter.js
+++ src/js/cocktailByLetter.js
@@ -28,13 +28,14 @@ function renderCards(cardsArray) {
     }
     renderMarkup(cardsArray);
   }
+}
 
-  function renderMarkup(cardsArray) {
-    let cardMarkup = [];
-    for (let i = 0; i < cardsArray.length; i += 1) {
-      if (i < 9) {
-        console.log(cardsArray[i].strDrink);
-        cardMarkup.push(`<li class="card">
+function renderMarkup(cardsArray) {
+  let cardMarkup = [];
+  for (let i = 0; i < cardsArray.length; i += 1) {
+    if (i < 9) {
+      console.log(cardsArray[i].strDrink);
+      cardMarkup.push(`<li class="card">
       <img src="${cardsArray[i].strDrinkThumb}" loading="lazy" alt="${cardsArray[i].strDrink}" />
       <div class="card__info">
         <p class="card__title">${cardsArray[i].strDrink}</p>
@@ -51,35 +52,34 @@ function renderCards(cardsArray) {
         </div>
       </div>
     </li>`);
-      }
     }
-    cardsListEl.innerHTML = cardMarkup.join('');
   }
-
-  function markupToggle() {
-    voidMarkup.classList.toggle('is-hidden');
-    cardsSectionEl.classList.toggle('is-hidden');
-    paginationEl.classList.toggle('is-hidden');
-  }
-  // .map(
-  //   cardObj => `<li class="card">
-  //     <img src="${cardObj.strDrinkThumb}" alt="${cardObj.strDrink}" />
-  //     <div class="card__info">
-  //       <p class="card__title">${cardObj.strDrink}</p>
-  //       <div class="card__btns">
-  //         <button type="button" class="card__btn">
-  //           Learn more
-  //         </button>
-  //         <button type="button" class="card__btn-add">
-  //           Add to
-  //           <svg class="card__icon" width="18" height="18">
-  //             <use href="./images/icons.svg#icon-Heart"></use>
-  //           </svg>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </li>`
-  // )
-  //   .join('');
-  // cardsList.innerHTML = cardMarkup;
+  cardsListEl.innerHTML = cardMarkup.join('');
 }
+
+function markupToggle() {
+  voidMarkup.classList.toggle('is-hidden');
+  cardsSectionEl.classList.toggle('is-hidden');
+  paginationEl.classList.toggle('is-hidden');
+}
+// .map(
+//   cardObj => `<li class="card">
+//     <img src="${cardObj.strDrinkThumb}" alt="${cardObj.strDrink}" />
+//     <div class="card__info">
+//       <p class="card__title">${cardObj.strDrink}</p>
+//       <div class="card__btns">
+//         <button type="button" class="card__btn">
+//           Learn more
+//         </button>
+//         <button type="button" class="card__btn-add">
+//           Add to
+//           <svg class="card__icon" width="18" height="18">
+//             <use href="./images/icons.svg#icon-Heart"></use>
+//           </svg>
+//         </button>
+//       </div>
+//     </div>
+//   </li>`
+// )
+//   .join('');
+// cardsList.innerHTML = cardMarkup;
