d9e3614b0c2ad13ec5e8fe8367f3adf0f221cab9
Yuriy Minashkin <y.minashkin@gmail.com>
2023-01-03 23:24:21 +0200

    hero button js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './js/CocktailsApiService';
 import './js/randomCocktailsCards';
 import './js/cocktailByLetter';
-import './js/headerMain'
-import './js/changeOfSubject'
-import './js/modalMobile'
-import './js/searchHeader'
+import './js/headerMain';
+import './js/changeOfSubject';
+import './js/modalMobile';
+import './js/searchHeader';
 import './js/cocktailNotFound';
 import './js/firebase';
+import './js/heroButtonChange';
