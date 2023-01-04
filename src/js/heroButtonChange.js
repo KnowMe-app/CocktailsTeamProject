e41a511004d14b9ce2dcc9360b9ee4973ba6722f
Yuriy Minashkin <y.minashkin@gmail.com>
2023-01-04 14:20:34 +0200

    hero button update

--- src/js/heroButtonChange.js
+++ src/js/heroButtonChange.js
@@ -6,4 +6,10 @@ searchListEl.addEventListener('click', onLetterClickChangeBtnLetter);
 function onLetterClickChangeBtnLetter(event) {
   const letterSelected = event.target.name.toUpperCase();
   heroButtonEl.textContent = letterSelected;
+  searchListEl.style.transform = 'translateY(-100%)';
+  searchListEl.style.visibility = 'hidden';
+  setTimeout(() => {
+    searchListEl.style.removeProperty('transform');
+    searchListEl.style.removeProperty('visibility');
+  }, 50);
 }
