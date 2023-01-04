d00a07f9a7fe0165028d7d43c5beb44c45ea82f2
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-01-04 21:45:41 +0200

    04.01
    мобильное меню.

--- src/js/changeOfSubject.js
+++ src/js/changeOfSubject.js
@@ -6,8 +6,11 @@ const home = document.querySelector('.navigation__home')
 const dropBTN = document.querySelector('.dropbtn')
 const favorite = document.querySelector('.navigation__favorite')
 const card = document.querySelector('.dark-tema')
-
+const sliderMain = document.querySelector('.slider-main.round-main')
+const bodyMain=document.querySelector('.modal')
+const drow=document.querySelector('.dropdown-content')
 slider.addEventListener('click', changeTheme)
+sliderMain.addEventListener('click', changeThemeMain)
 function changeTheme() {
     if(document.body.classList.toggle("dark")){
         black.style.color="var(--var-accent-color)"
@@ -16,7 +19,7 @@ function changeTheme() {
         favorite.style.color="var(--var-primary-bg-color)"
         card.style.color="var(--var-primary-bg-color)"
         dropBTN.style.color="var(--var-accent-color)"
-        
+        drow.style.backgroundColor="var(--var-accent-color)"
     }else{
         light.style.color="var(--var-accent-color)"
         black.style.color="var(--var-primary-text-color)"
@@ -27,3 +30,23 @@ function changeTheme() {
     }
   }
   
+//   function changeThemeMain() {
+//     if(document.body.classList.toggle("dark")){
+//         bodyMain.style.background="var(--var-primary-text-color)"
+//         black.style.color="var(--var-accent-color)"
+//         light.style.color="var(--var-primary-bg-color)"
+//         home.style.color="var(--var-primary-bg-color)"
+//         favorite.style.color="var(--var-primary-bg-color)"
+//         card.style.color="var(--var-primary-bg-color)"
+//         dropBTN.style.color="var(--var-accent-color)"
+        
+//     }else{
+//         light.style.color="var(--var-accent-color)"
+//         black.style.color="var(--var-primary-text-color)"
+//         home.style.color="var(--var-primary-text-color)"
+//         favorite.style.color="var(--var-primary-text-color)"
+//         card.style.color="var(--var-primary-bg-color)"
+//         dropBTN.style.color="var(--var-primary-text-color)"
+//         bodyMain.style.backgroundColor="var(--var-accent-color)"
+//     }
+//   }
---
+++ src/js/searchMobileMain.js
@@ -0,0 +1,43 @@
+import { searchCocktailsByLetter,searchCocktailByName} from './CocktailsApiService';
+
+const headerSearchMain = document.querySelector('.search__form-main');
+const cardsList = document.querySelector('.markup-cards');
+
+headerSearchMain.addEventListener('input', onLetterClick);
+
+function onLetterClick(event) {
+    event.preventDefault();
+    let selectedLetter = event.target.value;
+  if(selectedLetter.length===1){
+  searchCocktailsByLetter(selectedLetter).then(value =>
+    renderCards(value.drinks)
+  )}else{searchCocktailByName(selectedLetter).then(value =>
+    renderCards(value.drinks))}
+}
+
+function renderCards(cardsArray) {
+  let cardMarkup = [];
+  for (let i = 0; i < cardsArray.length; i += 1) {
+    if (i < 9) {
+      console.log(cardsArray[i].strDrink);
+      cardMarkup.push(`<li class="card">
+      <img src="${cardsArray[i].strDrinkThumb}" alt="${cardsArray[i].strDrink}" />
+      <div class="card__info">
+        <p class="card__title">${cardsArray[i].strDrink}</p>
+        <div class="card__btns">
+          <button type="button" class="card__btn">
+            Learn more
+          </button>
+          <button type="button" class="card__btn-add">
+            Add to
+            <svg class="card__icon" width="18" height="18">
+              <use href="./images/icons.svg#icon-Heart"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </li>`);
+    }
+  }
+  cardsList.innerHTML = cardMarkup.join('');
+}
