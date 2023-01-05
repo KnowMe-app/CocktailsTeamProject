9ea99c8a67d39d5375cfdde723fe2998b845ed7e
xanderkiev2022 <301019a@gmail.com>
2023-01-05 23:17:06 +0200

    renderPage2

--- src/js/searchMobileMain.js
+++ src/js/searchMobileMain.js
@@ -1,5 +1,9 @@
-import { searchCocktailsByLetter,searchCocktailByName} from './CocktailsApiService';
-import { renderPage, clearPagination } from './pagination';
+import {
+  searchCocktailsByLetter,
+  searchCocktailByName,
+} from './CocktailsApiService';
+// import { renderPage, clearPagination } from './pagination';
+import { clearPagination } from './pagination';
 const headerSearchMain = document.querySelector('.search__form-main');
 // const cardsList = document.querySelector('.markup-cards');
 const paginationEl = document.querySelector('.cards__pagination');
@@ -9,19 +13,25 @@ const voidMarkup = document.querySelector('.void');
 headerSearchMain.addEventListener('input', onLetterClick);
 
 function onLetterClick(event) {
-    event.preventDefault();
-    let selectedLetter = event.target.value;
-  if(selectedLetter.length===1){
-  searchCocktailsByLetter(selectedLetter).then(value =>{
-    clearPagination()
-    renderMarkup(value.drinks)
+  event.preventDefault();
+  let selectedLetter = event.target.value;
+  if (selectedLetter.length === 1) {
+    searchCocktailsByLetter(selectedLetter).then(
+      value => {
+        clearPagination();
+        renderMarkup(value.drinks);
+      }
+      // renderCards(value.drinks)
+    );
+  } else {
+    searchCocktailByName(selectedLetter).then(value =>
+      // renderCards(value.drinks)
+      {
+        clearPagination();
+        renderMarkup(value.drinks);
+      }
+    );
   }
-    // renderCards(value.drinks)
-  )}else{searchCocktailByName(selectedLetter).then(value =>
-    // renderCards(value.drinks)
-    { clearPagination()
-      renderMarkup(value.drinks)}
-    )}
 }
 
 // function renderCards(cardsArray) {
